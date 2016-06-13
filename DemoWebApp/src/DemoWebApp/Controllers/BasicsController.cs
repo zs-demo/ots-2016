404e4be861fea442e63a3dece9e3dfd67bf55a57
zs-demo <zedins@gmx.com>
2016-06-13 22:46:47 +0200

    starting project

---
+++ DemoWebApp/src/DemoWebApp/Controllers/BasicsController.cs
@@ -0,0 +1,56 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DemoWebApp.Controllers
+{
+    public class BasicsController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult AspNetCore()
+        {
+            return View();
+        }
+
+        public IActionResult Structure()
+        {
+            return View();
+        }
+
+        public IActionResult EF7()
+        {
+            return View();
+        }
+
+        public IActionResult EFCore()
+        {
+            return View();
+        }
+
+        public IActionResult TagHelpers()
+        {
+            return View();
+        }
+
+        public IActionResult Areas()
+        {
+            return View();
+        }
+
+        public IActionResult Controllers()
+        {
+            return View();
+        }
+
+        public IActionResult Migrations()
+        {
+            return View();
+        }
+    }
+}
--- DemoWebApp/src/DemoWebApp/wwwroot/js/site.js
+++ DemoWebApp/src/DemoWebApp/wwwroot/js/site.js
@@ -1,1 +1,30 @@
 ﻿// Write your Javascript code.
+
+$('.nav-main .nav-submenu').on('click', function () {
+    var c = $(this);
+    var d = c.parent("li");
+
+    if (d.hasClass("open")) {
+        d.removeClass("open");
+    } else {
+        c.closest("ul").find("> li").removeClass("open");
+        d.addClass("open");
+    }
+});
+
+$('button[data-action=sidebar_mini_toggle]').on('click', function () {
+    var b = $('#page-container');
+    b.toggleClass('sidebar-mini');
+});
+
+function updateContainer() {
+    var m = $(window).height();
+    var x = (m - $('.side-header').outerHeight());
+    $('#sidebar').height(x);
+}
+
+$(document).ready(function () {
+    $('.nav-main a').removeClass('active');
+    $('.nav-main a[data-tmp=' + $('.page-heading').attr('data-tmp') + ']').addClass('active');
+
+});
